60400ea9911fec971d448af99614a01f9d46a1bb
shinji.misaka <shinmisa0114@gmail.com>
2019-07-03 18:06:34 +0900

    ラベルの追加と画像の追加をした。 条件分けで進むと戻るボタンを作成した。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let images = [UIImage(named: "apple"),UIImage(named: "onigiri"),UIImage(named: "lemon")]
+    var imageIndex = 0
+    
+    
+    
+    @IBOutlet weak var Imageview: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Imageview.image=images[0]
     }
 
-
+    @IBAction func next(_ sender: Any) {
+        if imageIndex==2 {
+            imageIndex=0
+        } else {
+            imageIndex+=1
+        }
+        Imageview.image = images[imageIndex]
+    }
+    
+    @IBAction func back(_ sender: Any) {
+        if imageIndex==0 {
+            imageIndex=2
+        } else {
+            imageIndex-=1
+        }
+        Imageview.image = images[imageIndex]
+    }
+    
+    @IBAction func startstop(_ sender: Any) {
+    }
 }
 
